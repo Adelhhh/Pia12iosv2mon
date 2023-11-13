b82705ffcab2299288b83ba2f23f9ce5d05e85cb
Adel <adelhassanpour.ux@gmail.com>
2023-11-13 15:30:56 +0100

    Initial Commit

---
+++ Pia12iosv2mon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pia12iosv2mon
+//
+//  Created by Adel Hassanpour on 2023-11-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hej")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pia12iosv2mon/Pia12iosv2monApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pia12iosv2monApp.swift
+//  Pia12iosv2mon
+//
+//  Created by Adel Hassanpour on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct Pia12iosv2monApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
